b8549d3d78325dc955c98f36fcf65a765a8f1703
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-06-20 07:07:33 +0000

    [Finish] 4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,8 +11,18 @@ class BlogsController < ApplicationController
   end
 
   def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
   end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:titile, :category, :body)
+  end
+
+end
 
   def edit
   end
-end
+
