c4071aeeba1bb5f861554e89db9336c924a15b22
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-06-20 06:59:35 +0000

    [Finish] 4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,16 @@
 class BlogsController < ApplicationController
   def index
+  	@blogs = Blog.all
   end
 
   def show
   end
 
   def new
+  	@blog = Blog.new
+  end
+
+  def create
   end
 
   def edit
